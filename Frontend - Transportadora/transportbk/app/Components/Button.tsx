97f6a9ae4996ec6282667002d7bc6c97d8069028
hora-keven <kevensantos993@gmail.com>
2024-03-14 16:56:33 -0300

    fix: some bugs inside of button code

--- Frontend - Transportadora/transportbk/app/Components/Button.tsx
+++ Frontend - Transportadora/transportbk/app/Components/Button.tsx
@@ -1,15 +1,26 @@
-import { Text, TouchableOpacity } from "react-native";
+import {  PressableProps, Text, TouchableOpacity, TouchableOpacityProps, View } from "react-native";
 import styles from "../css/global";
 import { Feather } from "@expo/vector-icons";
-import { useNavigation } from "expo-router";
+
+import React from "react";
 
 
-function Button ({text, colorText, func, size, width, top, backgroundColor}){
-    const navigation = useNavigation()
+interface ButtonProps extends PressableProps {
+    text?: string,
+    colorText?: string,
+    size?: number,
+    width?: string,
+    top?: string,
+    backgroundColor?: string,
+
+}
+
+const Button = ({text, colorText, size, width, top, backgroundColor, ...rest}:ButtonProps)=>{
     return(
-        <TouchableOpacity style={[styles.button, {width:width, top:top, backgroundColor:backgroundColor == null?"#009898":backgroundColor}]} onPress={()=> navigation.navigate(func)}>
+        // style={[styles.input, {backgroundColor:bgColor, color:textColor}]}
+        <TouchableOpacity {...rest} style={[styles.button, {width:width, top:top, backgroundColor:backgroundColor || "#009898"}]}>
             <Text style={{color:colorText}}>{text}</Text>
-            <Feather style={{marginLeft:10}} name="arrow-right" color={"white"} size={size} / >
+            <Feather style={{marginLeft:10}} name="arrow-right" color={"white"} size={size} />
         </TouchableOpacity>
     )
 }
