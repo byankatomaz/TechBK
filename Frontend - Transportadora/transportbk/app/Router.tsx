5305711943c0eb6d5a020c52c753921149aa8c96
hora-keven <kevensantos993@gmail.com>
2024-03-14 14:19:19 -0300

    feat: updated some parts in the code

--- Frontend - Transportadora/transportbk/app/Router.tsx
+++ Frontend - Transportadora/transportbk/app/Router.tsx
@@ -1,11 +1,10 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
-import React, {useState} from "react";
+import React from "react";
 import Welcome from "./Screen/Welcome";
 import Login from "./Screen/Login";
-
-import Register from "./Screen/Resgister";
+import Register from "./Screen/Register";
 import Home from "./Screen/Home";
 import Requests from './Screen/Requests' 
 import DeliveryDone from "./Screen/DeliveryDone";
@@ -17,20 +16,20 @@ const stack = createStackNavigator();
   
   
     return(
-        <>
-    <NavigationContainer independent={true}>
-        <TechBkProvider>
-        <stack.Navigator>
-        <stack.Screen name="DeliveryDone" component={DeliveryDone}  options={{headerShown:false}}/>
-        <stack.Screen name="Welcome" component={Welcome}  options={{headerShown:false}}/>
-      
-        <stack.Screen name="Home" component={Home}  options={{headerShown:false}}/>
-        <stack.Screen name="Login" component={Login}  options={{headerShown:false}}/>
-        <stack.Screen name="Register" component={Register}  options={{headerShown:false}}/>
-        <stack.Screen name="Requests" component={Requests}  options={{headerShown:false}}/>
-        </stack.Navigator>
-        </TechBkProvider>
-    </NavigationContainer>
+    <>
+        <NavigationContainer independent={true}>
+            <TechBkProvider>
+            <stack.Navigator>
+         
+            <stack.Screen name="Welcome" component={Welcome}  options={{headerShown:false}}/>
+            <stack.Screen name="DeliveryDone" component={DeliveryDone}  options={{headerShown:false}}/>
+            <stack.Screen name="Home" component={Home}  options={{headerShown:false}}/>
+            <stack.Screen name="Login" component={Login}  options={{headerShown:false}}/>
+            <stack.Screen name="Register" component={Register}  options={{headerShown:false}}/>
+            <stack.Screen name="Requests" component={Requests}  options={{headerShown:false}}/>
+            </stack.Navigator>
+            </TechBkProvider>
+        </NavigationContainer>
     </>
     )
     
