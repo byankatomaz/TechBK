747c5144f396c8cde6609ba0bcb4fc0e2043ed51
hora-keven <kevensantos993@gmail.com>
2024-03-16 18:34:15 -0300

    fix: some changes done

--- Frontend - Transportadora/transportbk/app/Screen/Requests.tsx
+++ Frontend - Transportadora/transportbk/app/Screen/Requests.tsx
@@ -1,26 +1,24 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import styles from "../css/global";
 import Card from "../Components/Card";
-import StatusBar from "../Components/StatusBar";
 import ContainerInfo from "../Components/ContainerInfo";
-
+import React from "react";
 
 export default function Requests() {
     return (
-        <>
-        <StatusBar/>
-            <View style={styles.container}>
-                <ContainerInfo text={"Pedidos"} icon={"box"}/>
-                <View style={{ width: "100%", display: "flex", alignItems: "center", height: "70%" }}>
+
+        <View style={styles.container}>
+            <ContainerInfo text={"Pedidos"} icon={"box"} />
+            <View style={{ width: "100%", display: "flex", alignItems: "center", height: "70%" }}>
 
                 <View style={styles.cards}>
-                    <TouchableOpacity onPress={console.log("aaaaaa")}>
-                        <Card nome={"MARIA ANTONIA DE APARECISA DSE"}/>
+                    <TouchableOpacity>
+                        <Card nome={"MARIA ANTONIA DE APARECISA DSE"} />
                     </TouchableOpacity>
                 </View>
-                </View>
-
             </View>
-        </>
+
+        </View>
+
     )
 }
