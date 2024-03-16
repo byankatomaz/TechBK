f26e55e2f7ba452bbdb9424b988bf025942f5c5d
hora-keven <kevensantos993@gmail.com>
2024-03-16 18:31:52 -0300

    fix: some changes done

--- Frontend - Transportadora/transportbk/app/Components/CardDetails.tsx
+++ Frontend - Transportadora/transportbk/app/Components/CardDetails.tsx
@@ -1,14 +1,14 @@
 import { View, Text, TouchableOpacity } from "react-native"
 import styles from "../css/global"
-
+import React from "react"
 interface CardDetailsProps{
-    nome: string,
-    codigo: string
+    nome?: string,
+    codigo?: string
 }
 export default function CardDetails({nome, codigo}:CardDetailsProps){
-  
+
     return(
- 
+
             <View style={styles.cardDetails}>
                 <View style={{display:"flex", flexDirection:"row",  width:"100%",height:25, justifyContent:"center", bottom:10, borderBottomWidth:1, borderBottomColor:"#009898"}}>
                     <Text style={{color:"white", paddingRight:40}}>Nome</Text>
@@ -17,10 +17,8 @@ export default function CardDetails({nome, codigo}:CardDetailsProps){
                 <View style={{display:"flex", flexDirection:"row"}}>
                     <Text style={{color:"white", paddingRight:40}}>{nome}</Text>
                     <Text style={{color:"white", paddingLeft:20}}>{codigo}</Text>
-                  
                 </View>
         </View>
 
-       
     )
 }
