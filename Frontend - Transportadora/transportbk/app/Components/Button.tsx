24fa232e1084acc1710127f5e4c9be1bf2c5a730
hora-keven <kevensantos993@gmail.com>
2024-03-16 18:30:03 -0300

    fix: correct way to make a component using typescript

--- Frontend - Transportadora/transportbk/app/Components/Button.tsx
+++ Frontend - Transportadora/transportbk/app/Components/Button.tsx
@@ -1,27 +1,42 @@
-import {  PressableProps, Text, TouchableOpacity, TouchableOpacityProps, View } from "react-native";
+import {  PressableProps, Text, Pressable,View } from "react-native";
 import styles from "../css/global";
 import { Feather } from "@expo/vector-icons";
-
+import { IconProps } from "@expo/vector-icons/build/createIconSet";
 import React from "react";
 
-
-interface ButtonProps extends PressableProps {
-    text?: string,
-    colorText?: string,
-    size?: number,
-    width?: string,
-    top?: string,
-    backgroundColor?: string,
-
-}
-
-const Button = ({text, colorText, size, width, top, backgroundColor, ...rest}:ButtonProps)=>{
-    return(
-        // style={[styles.input, {backgroundColor:bgColor, color:textColor}]}
-        <TouchableOpacity {...rest} style={[styles.button, {width:width, top:top, backgroundColor:backgroundColor || "#009898"}]}>
-            <Text style={{color:colorText}}>{text}</Text>
-            <Feather style={{marginLeft:10}} name="arrow-right" color={"white"} size={size} />
-        </TouchableOpacity>
-    )
-}
+interface CustomButtonProps extends PressableProps {
+    text?: string;
+    backgroundColor?: string;
+    colorText?: string;
+    size?: number;
+    width?: number;
+    top?: number;
+    iconName?:string
+  }
+  
+  const Button: React.FC<CustomButtonProps> = ({
+    text,
+    backgroundColor,
+    colorText,
+    size,
+    width,
+    top,
+    iconName,
+    ...rest
+  }) => {
+    return (
+      <Pressable
+        {...rest}
+        style={[
+          styles.button,
+          { width: width, top: top, backgroundColor: backgroundColor || "#009898" },
+        ]}
+      >
+        <Text style={{ color: colorText }}>{text}</Text>
+        <View style={{ marginLeft: 10 }}>
+          <Feather name={iconName} color={"white"} size={size} />
+        </View>
+      </Pressable>
+    );
+  };
 export default Button;
