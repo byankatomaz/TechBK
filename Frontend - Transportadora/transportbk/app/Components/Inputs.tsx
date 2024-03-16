bf5c671895fbe9e08dd9dc095bf7441c950cee43
hora-keven <kevensantos993@gmail.com>
2024-03-16 18:32:53 -0300

    fix: some changes done

--- Frontend - Transportadora/transportbk/app/Components/Inputs.tsx
+++ Frontend - Transportadora/transportbk/app/Components/Inputs.tsx
@@ -2,15 +2,16 @@
 import styles from "../css/global";
 import { TextInput } from "react-native-gesture-handler";
 import React from "react";
+import { TextInputProps } from "react-native";
 
-interface InputProps{
+interface InputProps extends TextInputProps {
     placeholder: string,
     bgColor: string,
-    textColor: string
+    textColor?: string
 }
-function Input ({placeholder, bgColor, textColor}:InputProps){
-    return(
-       <TextInput placeholder={placeholder} style={[styles.input, {backgroundColor:bgColor, color:textColor}]} placeholderTextColor={textColor}/>
+function Input({ placeholder, bgColor, textColor }: InputProps) {
+    return (
+        <TextInput placeholder={placeholder} style={[styles.input, { backgroundColor: bgColor, color: textColor }]} placeholderTextColor={textColor} />
     )
 }
 export default Input;
