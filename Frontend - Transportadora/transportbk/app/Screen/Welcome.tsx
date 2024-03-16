fa532dd57ba5a75f8002d61abcde7f73c0178759
hora-keven <kevensantos993@gmail.com>
2024-03-16 18:34:29 -0300

    fix: some changes done

--- Frontend - Transportadora/transportbk/app/Screen/Welcome.tsx
+++ Frontend - Transportadora/transportbk/app/Screen/Welcome.tsx
@@ -1,27 +1,27 @@
 
-import { View, Image ,} from "react-native";
+import { View, Image, } from "react-native";
 import styles from "../css/global";
 import logo from "./../../assets/images/logo.png";
 import Button from "../Components/Button";
-import StatusBar from "../Components/StatusBar";
+import React from "react";
 
 
-export default function Welcome({navigation}){
-   
-   
-    return(
-        <>
-        <StatusBar/>
+export default function Welcome({ navigation }: any) {
+
+
+    return (
+
+
         <View style={styles.container}>
-       
+
             <View style={styles.containerImg}>
                 <Image source={logo} style={styles.logoImage} />
             </View>
 
             <View>
-                <Button  text={"Bem-vindo(a) ao BK Transport"} colorText={"white"} func={"Login"} size={30} top={100} width={280}/>
+                <Button text={"Bem-vindo(a) ao BK Transport"} colorText={"white"} onPress={() => navigation.navigate("Login")} size={30} top={100} width={280} />
             </View>
         </View>
-        </>
+
     )
 }
