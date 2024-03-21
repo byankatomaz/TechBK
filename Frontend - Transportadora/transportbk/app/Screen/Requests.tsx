be6e66a5ca244c753c019411d8d87f4b8cfbd0fd
hora-keven <kevensantos993@gmail.com>
2024-03-21 15:36:10 -0300

    feat: nothing to update

--- Frontend - Transportadora/transportbk/app/Screen/Requests.tsx
+++ Frontend - Transportadora/transportbk/app/Screen/Requests.tsx
@@ -1,12 +1,12 @@
 import { View, Text, TouchableOpacity } from "react-native";
-import styles from "../css/global";
-import Card from "../Components/Card";
-import ContainerInfo from "../Components/ContainerInfo";
+import styles from "../css/Global";
+import Card from "../components/Card";
+import ContainerInfo from "../components/ContainerInfo";
 import React from "react";
 
 export default function Requests() {
-    return (
 
+    return (
         <View style={styles.container}>
             <ContainerInfo text={"Pedidos"} icon={"box"} />
             <View style={{ width: "100%", display: "flex", alignItems: "center", height: "70%" }}>
