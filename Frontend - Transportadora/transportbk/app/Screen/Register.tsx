bc4e4d83c592f4c4d370f260cdf8ccfe5ef598aa
hora-keven <kevensantos993@gmail.com>
2024-03-21 15:35:57 -0300

    feat: keyboard aware scroll view added in register screen

--- Frontend - Transportadora/transportbk/app/Screen/Register.tsx
+++ Frontend - Transportadora/transportbk/app/Screen/Register.tsx
@@ -1,30 +1,28 @@
 
 import { View, Image } from "react-native";
-import styles from "../css/global";
+import styles from "../css/Global";
 import logo from "./../../assets/images/logo.png";
-import Button from "../Components/Button";
-import Input from "../Components/Inputs";
+import Button from "../components/Button";
+import Input from "../components/Inputs";
 import React from "react";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 
 
 export default function Register() {
     return (
-
-        <View style={styles.container}>
-            <View style={styles.containerImg}>
-                <Image source={logo} style={[styles.logoImage, { bottom: 100 }]} />
+        <KeyboardAwareScrollView>
+            <View style={styles.container}>
+                <View style={styles.containerImg}>
+                    <Image source={logo} style={[styles.logoImage, { bottom: 100 }]} />
+                </View>
+                <View>
+                    <Input placeholder={"Digite seu email: "} bgColor={"white"} />
+                    <Input placeholder={"Digite seu número: "} bgColor={"white"} />
+                    <Input placeholder={"Digite sua senha: "} bgColor={"white"} />
+                </View>
+                <Button size={0} text={"Registrar"} colorText={"white"} width={150} top={40} />
             </View>
-
-            <View>
-                <Input placeholder={"Digite seu email: "} bgColor={"white"} />
-                <Input placeholder={"Digite seu número: "} bgColor={"white"} />
-                <Input placeholder={"Digite sua senha: "} bgColor={"white"} />
-            </View>
-
-            <Button size={0} text={"Registrar"} colorText={"white"} width={150} top={40} />
-        </View>
-
-
+        </KeyboardAwareScrollView>
     )
 }
