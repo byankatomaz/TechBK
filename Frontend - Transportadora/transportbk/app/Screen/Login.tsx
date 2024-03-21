cbbeab5732bdd86df976eeba45682e6ca327db0b
hora-keven <kevensantos993@gmail.com>
2024-03-21 15:35:31 -0300

    feat: added the keyboard aware scroll view to inputs

--- Frontend - Transportadora/transportbk/app/Screen/Login.tsx
+++ Frontend - Transportadora/transportbk/app/Screen/Login.tsx
@@ -1,10 +1,10 @@
 import { View, Image , KeyboardAvoidingView, Dimensions} from "react-native";
-import styles from "../css/global";
+import styles from "../css/Global";
 import Logo from "../../assets/images/logo.png";
-import Button from "../Components/Button";
-import Input from "../Components/Inputs";
+import Button from "../components/Button";
+import Input from "../components/Inputs";
 import React from "react";
-
+import {KeyboardAwareScrollView} from "react-native-keyboard-aware-scroll-view"
 
 export const width = Dimensions.get("window").width
 export const height = Dimensions.get("window").height
@@ -12,22 +12,19 @@ export const height = Dimensions.get("window").height
 export default function Login({navigation}:any){
 
     return(
-        <KeyboardAvoidingView behavior="padding" style={{flex:1}}>
+        <KeyboardAwareScrollView>
             <View style={styles.container}>
                 <View style={styles.containerImg}>
                     <Image source={Logo} style={[styles.logoImage, {bottom:100}]} />
                 </View>
-
                 <View>
                     <Input  placeholder={"Digite seu usuario: "} bgColor={"white"}/>
                     <Input   placeholder={"Digite sua senha: "} bgColor={"white"}/>
                 </View>
-                
                     <Button size={0} text={"Não possui login? Cadastre-se"} colorText={"white"} onPress={()=> navigation.navigate("Register")} backgroundColor={"transparent"} width={250} top={20}/>
-
                 <Button size={0} text={"Login"} colorText={"white"}  width={150} top={40} onPress={() => {navigation.navigate("Home")}}/>
             </View>
-            </KeyboardAvoidingView>
+        </KeyboardAwareScrollView>
     
     )
 }
