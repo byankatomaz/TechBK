a918fa10f9af12defe2b74b660c15b8b93edd08e
Hora-keven <kevensantos993@gmail.com>
2024-04-12 15:36:31 -0300

    fix: rename the screen Home to ProductList

--- Frontend - TechBK/techbk/src/Router.tsx
+++ Frontend - TechBK/techbk/src/Router.tsx
@@ -5,9 +5,10 @@ import React from "react";
 import Welcome from "./screen/Welcome";
 import Login from "./screen/Login";
 import Register from "./screen/Register";
-import Home from "./screen/Home";
+import Home from "./screen/ProductList";
 import TechBkProvider from "./context/TechBkContext";
 import StatusBar from "./components/StatusBar/StatusBar";
+import ProductDescription from "./screen/ProductDescription";
 
 const stack = createStackNavigator();
 
@@ -19,10 +20,12 @@ function Router(){
             <TechBkProvider>
             <StatusBar/>
                 <stack.Navigator>
-                <stack.Screen name="Home" component={Home}  options={{headerShown:false}}/>
+                <stack.Screen name="ProductList" component={Home}  options={({route}) =>({
+                    headerShown:false
+                })}/>
                     <stack.Screen name="Welcome" component={Welcome}  options={{headerShown:false}}/>
                     <stack.Screen name="Login" component={Login}  options={{headerShown:false}}/>
-                    
+                    <stack.Screen name="ProductDescription" component={ProductDescription} options={{headerShown:false}}/>
                   
                     <stack.Screen name="Register" component={Register}  options={{headerShown:false}}/>
                   
