0f11727dedf2448ca3b95cf294365e74b39355f5
hora-keven <kevensantos993@gmail.com>
2024-04-13 17:38:55 -0300

    fix: removed the keyboard scroll view

--- Frontend - TechBK/techbk/src/screen/Register.tsx
+++ Frontend - TechBK/techbk/src/screen/Register.tsx
@@ -4,6 +4,7 @@ import styles from "../css/global";
 
 import React from "react";
 import { useForm } from "react-hook-form";
+import stylesComponent from "../components/Inputs/styles";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import Button from "../components/Button/Button";
 import Input from "../components/Inputs/Inputs";
@@ -18,18 +19,18 @@ export default function Register() {
 
     
     return (
-        <KeyboardAwareScrollView>
+        
             <View style={styles.container}>
-                <View style={styles.containerImg}>
+                <View style={styles.containerImage}>
                     <Image source={require("./../../assets/logo.png")} style={[styles.image, { bottom: 100 }]} />
                 </View>
-                <View>
+                <View style={[stylesComponent.containerInput, {bottom:80}]}>
                     <Input  onChangeText={text => setValue("name", text)} placeholder={"Digite seu nome: "} bgColor={"white"} />
                     <Input onChangeText={text => setValue("email", text)} placeholder={"Digite seu número: "} bgColor={"white"} />
                     <Input onChangeText={text => setValue("password", text)} placeholder={"Digite sua senha: "} bgColor={"white"} />
                 </View>
-                <Button  onPress={handleSubmit(() => onSubmit(getValues()))} size={0} text={"Registrar"} colorText={"white"} width={150} top={40} />
+                <Button  onPress={handleSubmit(() => onSubmit(getValues()))} size={0} text={"Registrar"} colorText={"white"} width={"50%"} top={0} />
             </View>
-        </KeyboardAwareScrollView>
+      
     )
 }
