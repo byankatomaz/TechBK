f70024af4247a1db6e06c9be2c0fdff4b62aa9e4
hora-keven <kevensantos993@gmail.com>
2024-04-13 17:38:35 -0300

    fix: some imports of product details renamed

--- Frontend - TechBK/techbk/src/screen/ProductList.tsx
+++ Frontend - TechBK/techbk/src/screen/ProductList.tsx
@@ -2,20 +2,19 @@ import { Feather } from "@expo/vector-icons";
 import React, { useContext, useState } from "react";
 import { Image,  Text, TouchableOpacity, View } from "react-native";
 import { FlatList, ScrollView } from "react-native-gesture-handler";
-import ContainerInfo from "../components/TopHeader/TopHeader";
 import styles from "../css/global";
 import cell from '../../assets/cell.png';
 import Product from "../interfaces/Product";
-import { TechContext } from "../context/TechBkContext";
-import ModalInfo from '../components/Modal/Modal';
+import TopHeader from "../components/TopHeader/TopHeader";
 
 export default function ProductList({ navigation }: any) {
-  const[product, setProduct] = useState<Product>()
+  
   
   type DataType = {
     id: string;
     name: string;
     price: string;
+    brand: string
 
   }[];
 
@@ -23,34 +22,39 @@ export default function ProductList({ navigation }: any) {
     {
       id: '1',
       name: "Galaxy A54",
-      price: "R$ 700,00",
+      price: "700",
+      brand:"Samsung"
 
     },
     {
       id: '2',
       name: "Galaxy A55",
-
-      price: "R$ 600,00",
+      price: "600",
+      brand:"Samsung"
     },
     {
       id: '3',
       name: "Galaxy A56",
-      price: "R$ 500,00",
+      price: "500",
+      brand:"Samsung"
     },
     {
       id: '4',
       name: "Gakaxy S23 FE",
-      price: "R$ 400,00",
+      price: "400",
+      brand:"Samsung"
     },
     {
       id: '5',
       name: "Iphone 13",
-      price: "R$ 300,00",
+      price: "300",
+      brand:"Samsung"
     },
     {
       id: '6',
       name: "Galaxy Z Fold5",
-      price: "R$ 200,00",
+      price: "200",
+      brand:"Samsung"
     },
   ];
 
@@ -58,26 +62,26 @@ export default function ProductList({ navigation }: any) {
 
   const Item = ({ name, productType, price }: ItemProps) => (
     <View style={styles.item}>
-      <View style={{ width: "100%",padding:2, display: "flex", alignItems: "flex-end", justifyContent: "center" }}>
+      <View style={styles.itemIcon}>
         <TouchableOpacity>
           <Feather name={"bookmark"} color={"gray"} size={30} />
         </TouchableOpacity>
       </View>
-      <View style={[styles.containerImg, { bottom: 0, height: 100 }]}>
+      <View style={[styles.containerImage, { bottom: 0, height: 100 }]}>
         <Image source={cell} style={styles.image} />
       </View>
 
       <View style={{ paddingTop: 20, width:"100%", display:"flex", alignItems:"center" }}>
         <Text style={[styles.text, {fontSize:10}]}>{productType}</Text>
         <Text style={[styles.text, {fontSize:20}]}>{name}</Text>
-        <Text style={[styles.text, {fontSize:15, color:"#009898"}]}>{price}</Text>
+        <Text style={[styles.text, {fontSize:15, color:"#009898"}]}>R$ {price}</Text>
       </View>
     </View>
   );
 
   return (
     <View style={styles.container}>
-      <ContainerInfo icon={"search"} />
+      <TopHeader typeTopHeader={true}  icon={"search"} />
       <View
         style={{
           width: "100%",
@@ -97,8 +101,16 @@ export default function ProductList({ navigation }: any) {
             renderItem={({ item }) => (
 
               <TouchableOpacity style={{ margin: 6.5 }} onPress={() => {
-                setProduct({name:item.name, price:item.price, quantity:4})
-                 navigation.navigate("ProductDescription", {product:product})}
+                const product:Product = {
+                  name:item.name,
+                  price: item.price,
+                  quantity:2,
+                  image:cell,
+                  brand:item.brand
+                }
+               navigation.navigate("ProductDetails", {
+                  product:product
+                })}
               }>
                 <Item name={item.name} price={item.price} productType={"cellphone"} />
               </TouchableOpacity>
