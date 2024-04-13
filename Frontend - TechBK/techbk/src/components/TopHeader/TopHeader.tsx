d6ba9cac22123a0e718cbaf286e0e2420e2d7e4a
hora-keven <kevensantos993@gmail.com>
2024-04-13 17:36:12 -0300

    fix: rename the container info to top header

--- Frontend - TechBK/techbk/src/components/TopHeader/TopHeader.tsx
+++ Frontend - TechBK/techbk/src/components/TopHeader/TopHeader.tsx
@@ -1,33 +1,50 @@
 import { Feather } from '@expo/vector-icons'
-import React, { useState } from 'react'
-import { Image, Text, TouchableOpacity, View } from 'react-native'
+import React, { useContext } from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
+
 import styles from './styles'
-import stylesG from '../../css/global'
 import Input from '../Inputs/Inputs'
+import ModalInfo from '../Modal/Modal'
+import { TechContext } from '../../context/TechBkContext'
 
 
 
-interface ContainerInfoProps{
+interface ToHeaderProps {
   text?: string,
-  icon: any
+  icon: any,
+  typeTopHeader?: boolean,
+  
 }
 
-const ContainerInfo = ({text, icon}:ContainerInfoProps) => {
-   
-  return (
-    <View style={styles.containerInfo}>
-      <Input bgColor={"#252525"} rigth={45} textColor='white'  placeholder={"Search prdoucts:"} bottom={19}/>
-        <TouchableOpacity>
-            <View style={{width:65, height:68, borderRadius:30,  display:"flex", alignItems:"center", justifyContent:"center"}}>
-                 <Feather name={icon} size={40} color={'white'}/>
-            </View>
-            </TouchableOpacity>
-
-            <View style={{marginLeft:14}}>
-              <Text style={styles.text}>{text}</Text>
-            </View>
+const TopHeader = ({ text, icon, typeTopHeader }: ToHeaderProps) => {
+  const {visible, changeVisible} = useContext(TechContext)
+  const TopHeaderProductList = () => (
+    <View style={styles.headers}>
+      <Input bgColor={"#252525"} textColor='white' placeholder={"Search prdoucts:"} bottom={19} />
+      <TouchableOpacity>
+        <View style={{ width: 65, height: 68, borderRadius: 30, display: "flex", alignItems: "center", justifyContent: "center" }}>
+          <Feather name={icon} size={30} color={'white'} />
         </View>
+      </TouchableOpacity>
+    </View>
+  )
+  const TopHeaderProductDetails = () => (
+    <View style={styles.headers}>
+     
+      <Text style={styles.text}>{text}</Text>
+      <TouchableOpacity onPress={()=>changeVisible(tr)}>
+        <View style={{ width: 65, height: 68, borderRadius: 30, display: "flex", alignItems: "center", justifyContent: "center" }}>
+          <Feather name={icon} size={30} color={'white'} />
+        </View>
+      </TouchableOpacity>
+    </View>
+  )
+  return (
+    <View style={styles.topheader}>
+      {typeTopHeader == true ? <TopHeaderProductList /> :<TopHeaderProductDetails/>}
+    <ModalInfo isVisible={visible}/>
+    </View>
   )
 }
 
-export default ContainerInfo;
+export default TopHeader;
