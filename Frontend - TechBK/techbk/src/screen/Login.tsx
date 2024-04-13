eec8d5b92ac3925cd08d38ea508b2b3272947d85
hora-keven <kevensantos993@gmail.com>
2024-04-13 17:37:38 -0300

    fix: removed keyboard scroll view

--- Frontend - TechBK/techbk/src/screen/Login.tsx
+++ Frontend - TechBK/techbk/src/screen/Login.tsx
@@ -11,19 +11,19 @@ export const height = Dimensions.get("window").height
 export default function Login({navigation}:any){
     
     return(
-        <KeyboardAwareScrollView>
+   
             <View style={styles.container}>
-                <View style={[styles.containerImg, {bottom:0}]}>
+                <View style={[styles.containerImage, {bottom:0}]}>
                     <Image source={require("./../../assets/logo.png")} style={[styles.image, {bottom:100}]} />
                 </View>
                 <View style={[stylesInput.containerInput, {bottom:40}]}>
                     <Input  placeholder={"Digite seu usuario: "} bgColor={"white"}/>
                     <Input   placeholder={"Digite sua senha: "} bgColor={"white"}/>
                 </View>
-                    <Button size={0} text={"Não possui login? Cadastre-se"} colorText={"white"} onPress={()=> navigation.navigate("Register")} backgroundColor={"transparent"} width={250} top={20}/>
-                <Button size={0} text={"Login"} colorText={"white"}  width={150} top={40} onPress={() => {navigation.navigate("ProductList")}}/>
+                    <Button size={0} text={"Não possui login? Cadastre-se"} colorText={"white"} onPress={()=> navigation.navigate("Register")} backgroundColor={"transparent"} width={"60%"} top={0}/>
+                <Button size={0} text={"Login"} colorText={"white"}  width={"50%"} top={0} onPress={() => {navigation.navigate("ProductList")}}/>
             </View>
-        </KeyboardAwareScrollView>
+      
     
     )
 }
