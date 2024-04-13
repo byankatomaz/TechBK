4ac48df750f451e2c6f11a25910b2a13b510b2a3
hora-keven <kevensantos993@gmail.com>
2024-04-13 17:35:26 -0300

    fix: renamed some imports used in this code

--- Frontend - TechBK/techbk/src/components/Modal/Modal.tsx
+++ Frontend - TechBK/techbk/src/components/Modal/Modal.tsx
@@ -1,10 +1,8 @@
 import React, { useContext } from 'react';
-import { Image, Modal, View } from 'react-native';
-import styles from '../Card/styles';
+import { Modal, View } from 'react-native';
 
 import { TechContext } from '../../context/TechBkContext';
 import Button from '../Button/Button';
-import CardDetails from '../Card/CardDetails';
 
 interface ModalProps {
     isVisible: boolean
@@ -13,16 +11,11 @@ interface ModalProps {
 const ModalInfo = ({ isVisible }: ModalProps) => {
     const { changeVisible } = useContext(TechContext)
     return (
-        <Modal presentationStyle={"overFullScreen"} transparent={true} style={{ display: "flex", justifyContent: "center", alignItems: "flex-end" }} animationType="slide" visible={isVisible} >
-            <View style={{ display: "flex", backgroundColor: "#252525", height: "60%", width: "90%", alignItems: "center", borderRadius: 7, left: 20, top: 130 }}>
+        <Modal transparent={true} animationType="slide" visible={isVisible} >
+            <View style={{ display: "flex", backgroundColor: "rgba(0, 0, 0, 0.6)", height: "100%", width: "100%", alignItems: "center", justifyContent: "center", borderRadius: 7, flex: 1 }}>
 
-                <View style={{ width: '100%', height: "50%", backgroundColor: "white" }}>
-                    <Image source={require("../../../assets/cell.jpeg")} style={{ width: "100%", height: "100%" }} />
-                </View>
-                <View style={[styles.cards, { width: "100%" }]}>
-                    <CardDetails />
-                </View>
-                <Button top={60} width={250} onPress={() => changeVisible(false)} text='Sair' colorText='white' />
+
+                <Button top={60} width={"50%"} onPress={() => changeVisible(false)} text={'Sair'} colorText='white' />
 
             </View>
         </Modal>
