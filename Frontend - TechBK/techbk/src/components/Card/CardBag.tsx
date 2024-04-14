d6364d27124277fe0e0f0e2a3dc1f7cade3b0af4
hora-keven <kevensantos993@gmail.com>
2024-04-14 16:05:42 -0300

    feat: rename the atribute amount

--- Frontend - TechBK/techbk/src/components/Card/CardBag.tsx
+++ Frontend - TechBK/techbk/src/components/Card/CardBag.tsx
@@ -1,12 +1,15 @@
-import React, { useState } from "react"
+import React, { useContext, useState } from "react"
 import { Image, Text, TouchableOpacity, View } from "react-native"
 import styles from "./styles"
 import Product from "../../interfaces/Product"
 import stylesGlobal from "../../css/global"
+import { TechContext } from "../../context/TechBkContext"
+
 
 export default function CardBag({ name, image, price }: Product) {
-    const [quantity, setQuantity] = useState<number>(1);
+    const [amount, setAmount] = useState<number>(1);
     const [total, setTotal] = useState<number>(price);
+    const {changeVisible} = useContext(TechContext)
     
     return (
 
@@ -17,15 +20,17 @@ export default function CardBag({ name, image, price }: Product) {
             <View style={{height:"100%",  width:"100%"}}>
                 <Text style={stylesGlobal.text}>{name}</Text>
                 <View style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: 85, borderRadius: 6, padding: 4, backgroundColor: "gray",marginTop:30 }}>
-                    <TouchableOpacity onPress={() => {setQuantity(quantity <= 1 ? quantity : (quantity - 1))
-                      setTotal((quantity-1)<=1?quantity*parseFloat(price):(quantity-1)*parseFloat(price))
+                    <TouchableOpacity onPress={() => {
+                    amount == 1 ?changeVisible(true):false
+                    setAmount(amount <= 1 ? amount : (amount - 1))
+                    setTotal((amount-1)<=1?amount*parseFloat(price):(amount-1)*parseFloat(price))
                     }}>
                         <Text style={stylesGlobal.text}>--</Text>
                     </TouchableOpacity>
-                    <Text style={stylesGlobal.text}>{quantity}</Text>
+                    <Text style={stylesGlobal.text}>{amount}</Text>
                     <TouchableOpacity onPress={() =>{ 
-                        setQuantity(quantity + 1)
-                        setTotal((quantity+1)*parseFloat(price))
+                        setAmount(amount + 1)
+                        setTotal((amount+1)*parseFloat(price))
                         }}>
                         
                         <Text style={stylesGlobal.text}>+</Text>
