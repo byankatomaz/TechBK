9256f9005226fa71a2ee14c0504e98f4ef0673da
hora-keven <kevensantos993@gmail.com>
2024-04-14 16:06:38 -0300

    fix: some atribute removed

--- Frontend - TechBK/techbk/src/components/Modal/Modal.tsx
+++ Frontend - TechBK/techbk/src/components/Modal/Modal.tsx
@@ -3,18 +3,23 @@ import { Modal, View } from 'react-native';
 
 import { TechContext } from '../../context/TechBkContext';
 import Button from '../Button/Button';
+import Product from '../../interfaces/Product';
+import Card from '../Card/Card';
 
 interface ModalProps {
-    isVisible: boolean
+    isVisible: boolean,
+    name:string,
+    price:string,
+    image:any
 }
 
-const ModalInfo = ({ isVisible }: ModalProps) => {
+const ModalInfo = ({ isVisible, name, price,image }: ModalProps) => {
     const { changeVisible } = useContext(TechContext)
     return (
         <Modal transparent={true} animationType="slide" visible={isVisible} >
             <View style={{ display: "flex", backgroundColor: "rgba(0, 0, 0, 0.6)", height: "100%", width: "100%", alignItems: "center", justifyContent: "center", borderRadius: 7, flex: 1 }}>
-
-
+                <View></View>
+                <Card name={name} image={image} price={price}/>
                 <Button top={60} width={"50%"} onPress={() => changeVisible(false)} text={'Sair'} colorText='white' />
 
             </View>
