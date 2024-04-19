325a7f998251db09c3839727b027a552b3956df6
hora-keven <kevensantos993@gmail.com>
2024-04-18 21:15:19 -0300

    fix: removed margins in styles

--- Frontend - TechBK/techbk/src/css/global.tsx
+++ Frontend - TechBK/techbk/src/css/global.tsx
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
         width: 185,
         borderRadius: 5,
         display: "flex",
-        height: 380,
+        height: 390,
         justifyContent: "center",
         alignItems: "center",
     },
