5127dd653a249ab22d9a1e49344fa303195748b3
hora-keven <kevensantos993@gmail.com>
2024-04-20 17:42:22 -0300

    ci: package-lock-json

---
+++ Frontend - TechBK/techbk/src/routes/Router.tsx
@@ -0,0 +1,127 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import 'react-native-gesture-handler'
+import React from "react";
+import StatusBar from "../components/StatusBar/StatusBar";
+import TechBkProvider from "../context/TechBkContext";
+import Bag from "../screen/Bag";
+import Login from "../screen/Login";
+import ProductDetails from "../screen/ProductDetails";
+import Home from "../screen/ProductList";
+import Register from "../screen/Register";
+import Welcome from "../screen/Welcome";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Feather } from "@expo/vector-icons";
+
+const stack = createStackNavigator();
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigator: React.FC = () => {
+    return (
+
+        <Tab.Navigator
+
+
+
+
+
+
+            screenOptions={({ route }) => ({
+                headerShown: false,
+
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName: any;
+
+                    if (route.name === 'Bag') {
+                        color = focused ? '#009898' : "gray";
+                        iconName = "shopping-cart"
+
+
+                    } else if (route.name === 'ProductList') {
+                        color = focused ? '#009898' : "gray";
+                        iconName = "home"
+                    }
+
+                    return <Feather name={iconName} size={20} color={color} />;
+                },
+
+
+                tabBarStyle: {
+                    backgroundColor: 'black',
+
+
+
+                },
+
+
+
+                tabBarItemStyle: {
+                    margin: 5,
+                }
+            })}
+            tabBarOptions={{
+                labelStyle: { fontSize: 14, fontWeight: 'bold' },
+                activeTintColor: '#009898',
+                inactiveTintColor: 'gray',
+            }}
+
+
+        >
+            <Tab.Screen
+                name="ProductList"
+                component={StachNav}
+                options={{
+                    tabBarLabel: 'Product List',
+
+                }}
+            />
+            <Tab.Screen
+                name="Bag"
+                component={Bag}
+                options={{
+                    tabBarLabel: 'Bag',
+
+
+                }}
+            />
+
+        </Tab.Navigator>
+    );
+};
+
+
+
+function StachNav() {
+
+    return (
+
+
+
+
+        <stack.Navigator>
+            <stack.Screen name="ProductList" component={Home} options={{ headerShown: false }} />
+            <stack.Screen name="Register" component={Register} options={{ headerShown: false }} />
+            <stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+            <stack.Screen name="Bag" component={Bag} options={{ headerShown: false }} />
+            <stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false }} />
+            <stack.Screen name="ProductDetails" component={ProductDetails} options={{ headerShown: false }} />
+        </stack.Navigator>
+
+
+
+    )
+
+
+}
+
+function Router() {
+    return (
+        <TechBkProvider>
+            <NavigationContainer>
+                <TabNavigator />
+            </NavigationContainer>
+        </TechBkProvider>
+    )
+}
+export default Router;
