0aa84e1d601790def7679f0aa229c0960b6acaf1
hora-keven <kevensantos993@gmail.com>
2024-04-20 17:40:56 -0300

    feat: new func bag added, to add product and remove product

--- Frontend - TechBK/techbk/src/context/TechBkContext.tsx
+++ Frontend - TechBK/techbk/src/context/TechBkContext.tsx
@@ -1,17 +1,22 @@
 import React, { ReactNode, createContext, useState } from 'react';
+import Product from '../interfaces/Product';
 
 interface TechContextProps {
   visible: boolean;
   filter: string;
+  bag: Product[],
   changeVisible: (isVisible: boolean) => void;
+  addProduct: (product: Product)=>void;
   findProducts: (product:any)=>void;
 }
 
 export const TechContext = createContext<TechContextProps>({
   visible: false,
   filter: "",
+  bag: [],
   changeVisible: () => {},
-  findProducts: ()=>{}
+  findProducts: ()=>{},
+  addProduct: ()=>{}
 });
 
 interface TechProviderProps {
@@ -21,16 +26,19 @@ interface TechProviderProps {
 function TechBkProvider({ children }: TechProviderProps) {
   const [visible, setVisible] = useState<boolean>(false);
   const [filter, setFilter] = useState<string>("celulares");
-
+  const [bag, setBag] = useState<Product[]>([]);
   function changeVisible(isVisible: boolean) {
     setVisible(isVisible);
   }
   function findProducts(product: string){
     setFilter(product)
   }
+  function addProduct(product: Product){
+    setBag([...bag, product])
+  }
 
   return (
-    <TechContext.Provider value={{ visible, changeVisible, findProducts, filter }}>
+    <TechContext.Provider value={{ addProduct, bag, visible, changeVisible, findProducts, filter }}>
       {children}
     </TechContext.Provider>
   );
