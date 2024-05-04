e06c5e66de62ac57ac8278bd08864d8530c66e42
hora-keven <kevensantos993@gmail.com>
2024-05-04 16:11:06 -0300

    fix: code format

--- Frontend - TechBK/techbk/src/screen/ProductList.tsx
+++ Frontend - TechBK/techbk/src/screen/ProductList.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Image,  SafeAreaView,  Text, TouchableOpacity, View } from "react-native";
+import { Image, SafeAreaView, Text, TouchableOpacity, View } from "react-native";
 import { FlatList, ScrollView } from "react-native";
 import styles from "../css/global";
 import Product from "../interfaces/Product";
@@ -11,87 +11,88 @@ import { ActivityIndicator } from "react-native";
 
 export default function ProductList({ navigation }: any) {
   const [products, setProducts] = useState<Product[]>([]);
-  const {filter} = useContext(TechContext)
-  
+  const { filter } = useContext(TechContext)
 
-  useEffect(()=>{
 
-    const response = ProductService.getProducts(filter).then(function(response){
-      const data = response.map((each:any)=>{
+  useEffect(() => {
+
+    const response = ProductService.getProducts(filter).then(function (response) {
+      const data = response.map((each: any) => {
         return each
       })
       setProducts(data)
     })
-    
-    
-  },[filter])
-  
+
+
+  }, [filter])
+
   const Item = ({ title, official_store_name, thumbnail, price }: Product) => (
     <View style={styles.item}>
-      <View style={[styles.containerImage, { bottom: 0, height: 150, width:150 }]}>
-        <Image source={{uri: thumbnail}} style={styles.image} />
+      <View style={[styles.containerImage, { bottom: 0, height: 150, width: 150 }]}>
+        <Image source={{ uri: thumbnail }} style={styles.image} />
       </View>
 
-      <View style={{padding: 10, width:"98%", height: '43%', display:"flex", alignItems:"center", justifyContent: 'center' }}>
-        <Text style={[styles.text, {fontSize:10}]}>{official_store_name}</Text>
-        <Text style={[styles.text, {fontSize:18}]}>{title}</Text>
+      <View style={{ padding: 10, width: "98%", height: '43%', display: "flex", alignItems: "center", justifyContent: 'center' }}>
+        <Text style={[styles.text, { fontSize: 10 }]}>{official_store_name}</Text>
+        <Text style={[styles.text, { fontSize: 18 }]}>{title}</Text>
       </View>
 
-      <View style={{width: '100%', height: '8%', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-        <Text style={[styles.text, {fontSize:18, fontWeight: 'bold', color:"#009898"}]}>R$ {price}</Text>
+      <View style={{ width: '100%', height: '8%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={[styles.text, { fontSize: 18, fontWeight: 'bold', color: "#009898" }]}>R$ {price}</Text>
       </View>
     </View>
   );
-  
+
   return (
     <View style={styles.container}>
-    
-      <TopHeader typeTopHeader={true}  icon={"search"} />
-    
+
+      <TopHeader typeTopHeader={true} icon={"search"} />
+
       <View
         style={{
           width: "100%",
           height: "80%",
-          marginTop:90,
+          marginTop: 90,
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-        
+
         }}
       >
-         
+
         <SafeAreaView>
-        {products.length == 0 && <ActivityIndicator hidesWhenStopped={true}/>}
+          {products.length == 0 && <ActivityIndicator hidesWhenStopped={true} />}
           <FlatList
             data={products}
             horizontal={false}
             numColumns={2}
             renderItem={({ item }) => (
-              
+
               <TouchableOpacity style={{ margin: 6.5 }} onPress={() => {
-                const product:Product = {
-                  title:item.title,
+                const product: Product = {
+                  title: item.title,
                   price: item.price,
-                  amount:1,
-                  thumbnail:item.thumbnail,
-                  official_store_name:item.official_store_name
+                  amount: 1,
+                  thumbnail: item.thumbnail,
+                  official_store_name: item.official_store_name
                 }
-              
-               navigation.navigate("ProductDetails", {
-                  product:product
-                })}
+
+                navigation.navigate("ProductDetails", {
+                  product: product
+                })
+              }
               }>
                 <Item title={item.title} price={item.price} thumbnail={item.thumbnail} official_store_name={item.official_store_name} />
               </TouchableOpacity>
             )}
             keyExtractor={(item) => item.id}
           />
-            
+
         </SafeAreaView>
-        
+
       </View>
-     
+
     </View>
-    
+
   );
 }
