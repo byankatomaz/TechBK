5673e653f416527ed50c56cbd8c6fcda8d3fe90a
hora-keven <kevensantos993@gmail.com>
2024-05-04 16:11:39 -0300

    feat: scrool view to use input

--- Frontend - TechBK/techbk/src/screen/User.tsx
+++ Frontend - TechBK/techbk/src/screen/User.tsx
@@ -2,45 +2,36 @@ import { View, Text, Image, ScrollView, SafeAreaView } from 'react-native'
 import React from 'react'
 import styles from '../css/global'
 import stylesComponent from '../components/TopHeader/styles'
-
+import stylesInput from '../components/Inputs/styles'
 import { TouchableOpacity } from 'react-native'
+import Input from '../components/Inputs/Inputs'
+import { height } from './Login'
 
 const User = () => {
     return (
         <View style={styles.container}>
-            <View style={[stylesComponent.topheader, { justifyContent: "flex-start" }]}>
+            <View style={[stylesComponent.topheader, { justifyContent: "flex-start"}]}>
                 <Text style={styles.text}>Usuário</Text>
             </View>
 
+            <ScrollView style={{  width: "100%", height:"100%", marginTop: 90 }}>
+            <SafeAreaView style={{ width: "100%", height: 650, display: "flex", justifyContent: "center", alignItems: "center",  }}>
 
-            <View style={{ width: "100%", height: "60%", display: "flex", justifyContent: "flex-start", alignItems: "center" }}>
 
-
-                <TouchableOpacity style={{ width: "50%", height: "45%", borderRadius: 200, display: "flex" }}>
+                <TouchableOpacity style={{ width: "50%", height: "30%", borderRadius: 200, display: "flex", marginBottom:12 }}>
                     <View>
                         <Image style={{ width: "100%", height: "100%", borderRadius: 200 }} source={require("../../assets/cell.jpeg")} />
                     </View>
                 </TouchableOpacity>
-                <View>
-                    <Text style={styles.text}>
-                        KEVEN
-                    </Text>
-                    <Text style={styles.text}>
-                        KEVEN
-                    </Text>
-                    <Text style={styles.text}>
-                        KEVEN
-                    </Text>
-                    <Text style={styles.text}>
-                        KEVEN
-                    </Text>
-                    <Text style={styles.text}>
-                        KEVEN
-                    </Text>
-                
+                <View style={[stylesInput.containerInput]}>
+                    <Input bgColor='white' placeholder={"keven"}/>
+                    <Input bgColor='white' placeholder={"keven"}/>
+                    <Input bgColor='white' placeholder={"keven"}/>
+                    <Input bgColor='white' placeholder={"keven"}/>
+                    <Input bgColor='white' placeholder={"keven"}/>
                 </View>
-            </View>
-
+            </SafeAreaView>
+    </ScrollView>
         </View>
     )
 }
