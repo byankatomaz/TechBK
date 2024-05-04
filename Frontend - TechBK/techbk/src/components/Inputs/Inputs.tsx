39744e1642c157a5f211c9f32cc8f707e839d14d
hora-keven <kevensantos993@gmail.com>
2024-05-04 16:08:41 -0300

    feat: width atribute added in INput interface

--- Frontend - TechBK/techbk/src/components/Inputs/Inputs.tsx
+++ Frontend - TechBK/techbk/src/components/Inputs/Inputs.tsx
@@ -10,12 +10,13 @@ interface InputProps extends TextInputProps {
     textColor?: string,
     bottom?:any,
     left?:any,
-    rigth?:any
+    rigth?:any,
+    width?:any
  
 }
-function Input({ placeholder, bgColor, left, rigth, textColor, bottom, ...rest }: InputProps) {
+function Input({ placeholder, bgColor, left, rigth, textColor, bottom,width, ...rest }: InputProps) {
     return (
-        <TextInput {...rest} placeholder={placeholder} style={[styles.input, { backgroundColor: bgColor, color: textColor, bottom:bottom, left:left, right:rigth, }]} placeholderTextColor={textColor} />
+        <TextInput {...rest} placeholder={placeholder} style={[styles.input, { backgroundColor: bgColor, color: textColor, bottom:bottom, left:left, right:rigth, width:width == null?250:width}]} placeholderTextColor={textColor} />
     )
 }
 export default Input;
