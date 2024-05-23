02f6dfff1f66aaa3a6123e6dc6824e01631ad6cf
Hora-keven <keven@gmail.com>
2024-05-23 16:51:13 -0300

    feat: add product inserted in product interface

--- Frontend - TechBK/techbk/src/services/ProductService.ts
+++ Frontend - TechBK/techbk/src/services/ProductService.ts
@@ -1,16 +1,19 @@
-import { apiProducts } from "../../api/ApiProducts";
+import { api, apiProducts } from "../../api/ApiProducts";
+import Product from "../interfaces/Product";
 
-const getProducts =async (product:string)=> {
+const getProducts =async (search:string)=> {
     
     try
     {
-     const response = await apiProducts.get(product) 
+     const response = await apiProducts.get(search) 
     return response.data.results
     }catch(error){
         console.error(error)
     }
 }
+const addProducts = (product:any)=> api.post("produto/", product)
 
 export const ProductService = {
-    getProducts
+    getProducts,
+    addProducts
 }
