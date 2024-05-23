975ce4503adf9ded278581f39b5d32f6c074d336
Hora-keven <keven@gmail.com>
2024-05-23 16:48:52 -0300

    feat: api added

--- Frontend - TechBK/techbk/api/ApiProducts.tsx
+++ Frontend - TechBK/techbk/api/ApiProducts.tsx
@@ -3,3 +3,6 @@ import axios from "axios";
 export const apiProducts = axios.create({
     baseURL:"https://api.mercadolibre.com/sites/MLB/search?q="
 }) 
+export const api = axios.create({
+    baseURL:"http://10.109.71.11:8000/api/v1/"
+})
