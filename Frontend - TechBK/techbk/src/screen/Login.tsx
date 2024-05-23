726514131d9b9d078ea63d22fd2294cf3b883a5b
Hora-keven <keven@gmail.com>
2024-05-23 16:50:36 -0300

    feat: login api

--- Frontend - TechBK/techbk/src/screen/Login.tsx
+++ Frontend - TechBK/techbk/src/screen/Login.tsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Dimensions, Image, View } from "react-native";
 import Button from "../components/Button/Button";
 import Input from "../components/Inputs/Inputs";
 import styles from "../css/global";
 import stylesInput from "../components/Inputs/styles";
+import { useForm } from "react-hook-form";
+import { UserService } from "../services/UserService";
+import {  LoginInterface } from "../interfaces/Login";
+import { TechContext } from "../context/TechBkContext";
+import { api } from "../../api/ApiProducts";
 export const width = Dimensions.get("window").width
 export const height = Dimensions.get("window").height
 
 export default function Login({ navigation }: any) {
 
+    const { handleSubmit, setValue, getValues } = useForm<LoginInterface>();
+    const { loggedIn } = useContext(TechContext)
+
+    const onSubmit = async () => {
+        let loginT:LoginInterface={
+            email:getValues("email"),
+            password: getValues("password")
+        }
+        const {status, data} = await UserService.login(loginT)
+        console.log(data.auth_token)
+        if(data.auth_token){
+            api.defaults.headers.Authorization = `Token ${data.auth_token}`;
+            loggedIn(data.auth_token)
+            navigation.navigate("ProductList")
+        
+        }
+  
+       
+    };
     return (
 
         <View style={styles.container}>
@@ -16,11 +40,11 @@ export default function Login({ navigation }: any) {
                 <Image source={require("./../../assets/logo.png")} style={[styles.image, { bottom: 100 }]} />
             </View>
             <View style={[stylesInput.containerInput, { bottom: 40 }]}>
-                <Input placeholder={"Digite seu usuario: "} bgColor={"white"} />
-                <Input placeholder={"Digite sua senha: "} bgColor={"white"} />
+                <Input placeholder={"Digite seu usuario: "} bgColor={"white"} onChangeText={(text)=>setValue("email", text)} />
+                <Input placeholder={"Digite sua senha: "} bgColor={"white"}  onChangeText={(text)=>setValue("password", text)}/>
             </View>
             <Button size={0} text={"Não possui login? Cadastre-se"} colorText={"white"} onPress={() => navigation.navigate("Register")} backgroundColor={"transparent"} width={"60%"} top={0} />
-            <Button size={0} text={"Login"} colorText={"white"} width={"50%"} top={0} onPress={() => { navigation.navigate("ProductList") }} />
+            <Button size={0} text={"Login"} colorText={"white"} width={"50%"} top={0} onPress={handleSubmit(onSubmit)} />
         </View>
 
 
